d333448e5bbd82aa50b2734b02e8c61ef9c4609d
Drew Maring <andrew.maring@gmail.com>
2019-02-06 16:34:29 -0800

    Add 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Prints "Programming is like building a multilingual puzzle
+ *
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	char string1[] = "\"Programming is like building a multilingual puzzle\n";
+
+	puts(string1);
+	return (0);
+}
