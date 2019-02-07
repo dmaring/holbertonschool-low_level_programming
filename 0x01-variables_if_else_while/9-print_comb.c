6f5b268a0d6f86c14b69cd09e338be9d47cb4c24
Drew Maring <andrew.maring@gmail.com>
2019-02-07 15:59:06 -0800

    add 9-print_combo.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Prints number 0 - 9 of base 10 separated by commas
+ *
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int nums[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(nums[i]);
+		if (nums[i] != '9')
+			putchar(',');
+	}
+	putchar('\n');
+
+	return (0);
+}
