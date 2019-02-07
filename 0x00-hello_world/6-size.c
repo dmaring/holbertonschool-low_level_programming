1ea3f4bb27522e31d223ef67f3508f434304309f
Drew Maring <andrew.maring@gmail.com>
2019-02-06 16:54:19 -0800

    Add 6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Prints out various type sizes for the machine
+ *
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int i;
+	char c;
+	long l;
+	long long ll;
+	float f;
+
+	printf("Size of a char: %i byte(s)\n", (int) sizeof(i));
+	printf("Size of a int: %i byte(s)\n", (int) sizeof(c));
+	printf("Size of a long int: %i byte(s)\n", (int) sizeof(l));
+	printf("Size of a long long int: %i byte(s)\n", (int) sizeof(ll));
+	printf("Size of a foat: %i byte(s)\n", (int) sizeof(f));
+
+	return (0);
+}
