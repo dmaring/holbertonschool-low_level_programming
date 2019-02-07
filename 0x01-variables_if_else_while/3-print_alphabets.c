1a8f722229668f814d36f5ced21a3ce1e2ae6b7d
Drew Maring <andrew.maring@gmail.com>
2019-02-07 15:45:04 -0800

    Add 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
+#include <string.h>
 
 /**
- * main - Prints lowercase letters of the alphabet using putchar()
+ * main - Prints lowercase letters of the alphabet in reverse using putchar()
  *
  * Return: 0 if successful
  */
 int main(void)
 {
-	char alpha[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ\n\0";
+	char alpha[] = "zyxwvutsrqponmlkjihgfedcba\0";
 	int i;
 
 	for (i = 0; alpha[i] != '\0'; i++)
 		putchar(alpha[i]);
+	putchar('\n');
 
 	return (0);
 }
