c3a4f8eedfaad2467a97e13f08eff8d5d23052fe
Drew Maring <andrew.maring@gmail.com>
2019-02-07 15:05:34 -0800

    Add 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints lowercase letters of the alphabet using putchar()
+ *
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	char alpha[] = "abcdfghijklmnoprstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ\n\0";
+	int i;
+
+	for (i = 0; alpha[i] != '\0'; i++)
+		putchar(alpha[i]);
+
+	return (0);
+}
