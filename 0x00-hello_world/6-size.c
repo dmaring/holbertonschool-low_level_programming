6714110b989d24e591143dad874ec8edcd0ce079
Drew Maring <andrew.maring@gmail.com>
2019-02-06 17:00:26 -0800

    Change to correct ouput order of 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,8 +13,8 @@ int main(void)
 	long long ll;
 	float f;
 
-	printf("Size of a char: %i byte(s)\n", (int) sizeof(i));
-	printf("Size of a int: %i byte(s)\n", (int) sizeof(c));
+	printf("Size of a char: %i byte(s)\n", (int) sizeof(c));
+	printf("Size of a int: %i byte(s)\n", (int) sizeof(i));
 	printf("Size of a long int: %i byte(s)\n", (int) sizeof(l));
 	printf("Size of a long long int: %i byte(s)\n", (int) sizeof(ll));
 	printf("Size of a foat: %i byte(s)\n", (int) sizeof(f));
