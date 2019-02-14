a0b4eeea5519c58742a15210766c1ac0f77ba83c
Drew Maring <andrew.maring@gmail.com>
2019-02-14 14:32:47 -0800

    Add 5-more_numbers.c

---
+++ 0x03-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - write numbers 0 to 14 ten times
+ *
+ */
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			if (j <= 9)
+				_putchar(j + '0');
+			else
+			{
+				_putchar((j / 10) + '0');
+				_putchar((j % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
