52f4396218199e5c91bec5e0e657199097554d37
Drew Maring <andrew.maring@gmail.com>
2019-02-13 21:29:02 -0800

    Add 104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdint.h>
+
+/**
+ * main - print first 98 Fibonacci numbers
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+	unsigned long a = 1;
+	unsigned long b = 2;
+	unsigned long c = 3;
+
+	printf("1, 2, ");
+	for (i = 0; i <= 96; i++)
+	{
+		c = a + b;
+		printf("%lu, ", c);
+		a = b;
+		b = c;
+	}
+	printf("\n");
+
+	return (0);
+}
