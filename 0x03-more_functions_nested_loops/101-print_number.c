ead8bae960ac18a8ef6c87adc2517064b867b496
Drew Maring <andrew.maring@gmail.com>
2019-02-15 16:09:05 -0800

    Change int type to signed in 101-print_number.c

--- 0x03-more_functions_nested_loops/101-print_number.c
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -7,12 +7,15 @@
  */
 void print_number(int n)
 {
-	if (n < 0)
-	{
-		_putchar('-');
-		n = -n;
-	}
-	if (n / 10)
-		print_number(n / 10);
-	_putchar(n % 10 + '0');
+	 signed long m = n;
+
+	 if (m < 0)
+	 {
+		 _putchar('-');
+		 m = -m;
+	 }
+
+	 if (m / 10)
+		print_number(m / 10);
+	_putchar(m % 10 + '0');
 }
