05aa5067df35ddac607dabdbe843e797bdad236c
Drew Maring <andrew.maring@gmail.com>
2019-02-19 11:15:12 -0800

    Add 0-reset_to_98.c

---
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - update the value in an array at position n
+ * @n: position in array to reset
+ *
+ * Return: always 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
