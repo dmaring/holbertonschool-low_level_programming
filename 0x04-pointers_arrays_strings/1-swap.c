381552f2780942cfa7a24107217c726a36164039
Drew Maring <andrew.maring@gmail.com>
2019-02-19 11:43:59 -0800

    Add 1-swap.c

---
+++ 0x04-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * swap_int - swaps value at pointer a to value at pointer b & vice versa
+ * @a: first pointer to swap values
+ * @b: second pointer to swap values
+ *
+ * Return: always 0
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
