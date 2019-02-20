89aae89204d373620f8fc38b90c14ee2d47be5d7
Drew Maring <andrew.maring@gmail.com>
2019-02-19 17:07:38 -0800

    Fix logic for printing half numbers in odd numbered strings

--- 0x04-pointers_arrays_strings/7-puts_half.c
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -7,30 +7,23 @@
  */
 void puts_half(char *str)
 {
-	int _strlen(char *s);
-	int i, len;
+	int len, i;
 
-	len = _strlen(str);
-	if (len % 2 == 0)
-		for (i = len / 2; str[i] != '\0'; i++)
-			_putchar(str[i]);
+	len = 0;
+
+	while (str[len] != '\0')
+		len++;
+
+	if ((len % 2) == 0)
+		i = (len / 2);
 	else
-		for (i = (len - 1) / 2; str[i] != '\0'; i++)
-			_putchar(str[i]);
-	_putchar('\n');
-}
-/**
- * _strlen - find and return the length of a string
- * @s: string input
- *
- * Return: return the number of items in a character array
- */
-int _strlen(char *s)
-{
-	int i;
+		i = ((len + 1) / 2);
 
-	i = 0;
-	while (s[i] != '\0')
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
 		i++;
-	return (i);
+	}
+
+	_putchar('\n');
 }
