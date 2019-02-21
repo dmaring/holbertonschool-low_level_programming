df398b75439f1da20be0be8da7a976564ec0b4da
Drew <andrew.maring@gmail.com>
2019-02-21 19:54:25 +0000

    Add 0-strcat.c

---
+++ 0x05-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,48 @@
+# include "holberton.h"
+
+/**
+ * _strcat - concatenate two strings
+n *
+ * @dest: destination string to add other string
+ * @src: source string to add on to destination string
+ *
+ * Return: pointer to resulting dest string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int _strlen(char *);
+	int mark = _strlen(dest);
+	int i;
+	char *p;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[mark] = src[i];
+		i++;
+		mark++;
+	}
+
+	dest[mark] = '\0';
+	p = dest;
+
+	return (p);
+}
+/**
+ * _strlen - calculate the length of a string
+ *
+ * @s: input string
+ *
+ * Return: integer of string length
+ */
+int _strlen(char *s)
+{
+	int length;
+
+	length = 0;
+	while (*(s + length) != '\0')
+		length++;
+
+	return length;
+}
