e1bab7f6f2affd8d67cc4bc4fba172fc75a372b8
Drew <andrew.maring@gmail.com>
2019-02-21 21:59:02 +0000

    Add 2-strncpy.c

---
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * _strncpy - copy a string from src to dest of n bytes
+ * @src: pointer to string to be copied
+ * @dest: pointer to string destination
+ * @n: number of bytes to copy from src string to dest string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i]; i++)
+		*(dest + i) = *(src + i);
+
+	while (i < n)
+	{
+		*(dest + i) = '\0';
+		i++;
+	}
+
+	return (dest);
+}
