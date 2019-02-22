6dad4e2efd22f5805d91b3054af14ec409020aa0
Drew <andrew.maring@gmail.com>
2019-02-22 01:47:40 +0000

    Add 8-rot13.c

---
+++ 0x05-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,27 @@
+# include "holberton.h"
+
+/**
+ * rot13 - convert string to ROT13
+ * @s: input string
+ *
+ * Return: pointer to modified array
+ */
+char *rot13(char *s)
+{
+	char input[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; input[j] != '\0'; j++)
+		{
+			if (s[i] == input[j])
+			{
+				s[i] = output[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
