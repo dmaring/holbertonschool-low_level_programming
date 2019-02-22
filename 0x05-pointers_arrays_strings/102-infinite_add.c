9c3d117b0346d723e445515b13e10edbf29e1acd
Drew <andrew.maring@gmail.com>
2019-02-22 04:26:46 +0000

    Add 102-infinite_add.c

---
+++ 0x05-pointers_arrays_strings/102-infinite_add.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * infinite_add - function that adds two numbers
+ * @n1 - 1st number
+ * @n2 - 2nd number
+ * @r - buffer to store the result
+ * @size_r - buffer size
+ */
+infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int i;
+
+	for (i = 0; i < size_r; i++)
+	{
+		r = n1 + n2;
+	}
+
+	return (&r);
+}
