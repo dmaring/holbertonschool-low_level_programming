5c0f3c6474134a8e5b5e6cab09818945189370a6
Drew <andrew.maring@gmail.com>
2019-02-25 19:42:23 +0000

    Add holberton.h and 0-memset.c

---
+++ 0x06-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * _memset - fills the first n bytes of memory pointed to by s
+ * with the constant byte b
+ * @s: pointer to memory position
+ * @b: constant byte to fill memory
+ * @n: number of bytes to fill
+ *
+ * Return: pointer to memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < n)
+	{
+		*(s + i) = b;
+		i++;
+	}
+
+	return (s);
+}
