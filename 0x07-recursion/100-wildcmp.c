a4031e8b1ca24373b05cfb802f62ef1437022eac
Drew <andrew.maring@gmail.com>
2019-03-02 03:06:19 +0000

    Refactor 100-wildcmp.c

---
+++ 0x07-recursion/100-wildcmp.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * wildcmp - compare 2 strings
+ * @s1: string 1
+ * @s2: string 2 with wildcards
+ *
+ * Return: 1 if strings identical, 0 if not
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0')
+	{
+		if (*s2 == '*')
+			return wildcmp(s1, s2 + 1);
+		return (*s2 == '\0');
+	}
+	if (*s2 == '*')
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	if (*s1 == *s2)
+		return wildcmp(s1 + 1, s2 + 1);
+	return (0);
+}
