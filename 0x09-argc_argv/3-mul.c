1e067e4f70735fe942015775b4f0a9944280a54c
Drew <andrew.maring@gmail.com>
2019-03-04 04:16:08 +0000

    Add 3-mul.c

---
+++ 0x09-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program that multiplies 2 numbers
+ * @argc: number of arguments passed in CLI
+ * @argv: vector of pointers to string arguments
+ *
+ * Return: 0 is successul, 1 if unsuccessful
+ */
+int main(int argc, char *argv[])
+{
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		printf("%i\n", (atoi(argv[1]) * atoi(argv[2])));
+		return (0);
+	}
+}
