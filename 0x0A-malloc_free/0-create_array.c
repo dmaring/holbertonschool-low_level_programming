0bf0ff0fcb17e4fbf39b0f4711b9807045d40442
Drew <andrew.maring@gmail.com>
2019-03-05 21:21:10 +0000

    Add 0-create_array.c holberton.h README.md

---
+++ 0x0A-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+
+/**
+ * create_array - create an array using malloc to allocate memory
+ * @size: size of the array
+ * @c: character to fill the array
+ *
+ * Return: pointer to array, NULL if failure
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *p;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+	
+	p = malloc(size * sizeof(char));
+
+	for (i = 0; i < size; i++)
+		p[i] = c;
+
+	if (p == NULL)
+		return NULL;
+
+	return (p);
+}
