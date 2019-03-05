e1b039f3311cf546337c0736e1c820f16ef41f3e
Drew <andrew.maring@gmail.com>
2019-03-05 22:25:56 +0000

    Add 1-strdup.c

---
+++ 0x0A-malloc_free/1-strdup.c
@@ -0,0 +1,43 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - copy string into new string in memory
+ * @str: input string to copy
+ *
+ * Return: pointer to new copied string, if string empty NULL
+ */
+char *_strdup(char *str)
+{
+	char *p;
+	int size = 0;
+	int i = 0;
+
+	if (!str)
+	{
+		return (NULL);
+	}
+
+	i = 0;
+	while (*(str + i))
+	{
+		size++;
+		i++;
+	}
+
+
+	p = malloc(size * sizeof(char));
+	if (p == NULL)
+		return (NULL);
+
+	i = 0;
+	while (*(str + i))
+	{
+		*(p + i) = *(str + i);
+		i++;
+	}
+
+	p[i] = '\0';
+
+	return (p);
+}
