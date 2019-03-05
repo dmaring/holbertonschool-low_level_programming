eeda5332febeae25c9fcd8feb6a2e31bf1fdb528
Drew <andrew.maring@gmail.com>
2019-03-05 21:29:01 +0000

    Change order of pointer NULL check

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -19,11 +19,12 @@ char *create_array(unsigned int size, char c)
 
 	p = malloc(size * sizeof(char));
 
+	if (p == NULL)
+		return (NULL);
+
 	for (i = 0; i < size; i++)
 		p[i] = c;
 
-	if (p == NULL)
-		return (NULL);
 
 	return (p);
 }
