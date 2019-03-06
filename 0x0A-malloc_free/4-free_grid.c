f35cfa45ba51c3492e1025a2e01391f6e992848b
Drew <andrew.maring@gmail.com>
2019-03-06 02:38:31 +0000

    Add 4-free_grid.c

---
+++ 0x0A-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * free_grind - frees a 2 dimensional grid from memory
+ * @grid: input 2 dim grid as double pointer
+ * @height: input height as integer
+ *
+ * Return: void
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+
+	free(grid);
+}
+	
