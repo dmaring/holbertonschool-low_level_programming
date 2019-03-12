5f982d1a76c986391d2fb866a5e666f79e5fdc6f
Drew <andrew.maring@gmail.com>
2019-03-12 03:22:24 +0000

    Add 1-init_dog.c, update dog.h with prototype

---
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ * init_dog - initialize a dog using struct in dog.h
+ * @d: pointer to structure being initilialized
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: dog's owner
+ *
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
