24e6f50f8c872107c1ef5b8fd8709fafa78a3ceb
Drew <andrew.maring@gmail.com>
2019-03-12 03:46:49 +0000

    Add additional conditional statements 2-print_dog.c

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -8,10 +8,16 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d)
+	if (d != NULL)
 	{
-		printf("Name: %s\n", d->name);
+		if (d->name != NULL)
+			printf("Name: %s\n", d->name);
+		else
+			printf("Name: (nil)\n");
 		printf("Age: %.6f\n", d->age);
-		printf("Owner: %s\n", d->owner);
+		if (d->owner != NULL)
+			printf("Owner: %s\n", d->owner);
+		else
+			printf("Owner: (nil)\n");
 	}
 }
