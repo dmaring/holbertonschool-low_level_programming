c470dc7db046d3ca522617eae303988556221b2a
Drew <andrew.maring@gmail.com>
2019-03-12 03:00:55 +0000

    Add README.md, dog.h

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - structure to hold dog related elements
+ *
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
