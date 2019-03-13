5f80bfec3834caf968c2a72049b20a3e2442054b
Drew <andrew.maring@gmail.com>
2019-03-13 20:58:41 +0000

    Add README.md function_pointers.h 0-print_name.c

---
+++ 0x0E-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - print the name of input string with input function
+ * @name: input name string
+ * @f: pointer to function to print string
+ *
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
