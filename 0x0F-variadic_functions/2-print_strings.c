775e80a09580febb3057003ccbbf398626251fc5
Drew <andrew.maring@gmail.com>
2019-03-17 01:22:45 +0000

    Add condition for NULL pointers passed to function in 2-print_strings.c

--- 0x0F-variadic_functions/2-print_strings.c
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -14,6 +14,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	unsigned int i = 0;
 	/** create variable of type va_list **/
 	va_list argList;
+	va_list cpyList;
 
 	/** initialize argList pointer using va_start **/
 	va_start(argList, n);
@@ -22,13 +23,21 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		/** if not pointer not NULL **/
-		if (argList)
+		va_copy(cpyList, argList);
+		if (va_arg(cpyList, char *) != NULL)
+		{
 			printf("%s", (va_arg(argList, char *)));
+		}
 		else
+		{
 			printf("(nil)");
+			va_arg(argList, char*);
+		}
 		/** if separator not NULL and argument is not last arg **/
 		if (separator && i < n - 1)
 			printf("%s", separator);
+
+		va_end(cpyList);
 	}
 
 	printf("\n");
