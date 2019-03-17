a5789fa9793053b06ca45250b4aca30370047211
Drew <andrew.maring@gmail.com>
2019-03-17 01:53:49 +0000

    Change 3-print_all.c to use switch statements

--- 0x0F-variadic_functions/3-print_all.c
+++ 0x0F-variadic_functions/3-print_all.c
@@ -1,6 +1,4 @@
-#include <stdarg.h>
-#include <stdio.h>
-#include "variadic_function.h"
+#include "variadic_functions.h"
 
 /**
  * print_all - function that prints everything
@@ -10,62 +8,40 @@
  */
 void print_all(const char * const format, ...)
 {
-	int argLength = 0;
-	int i = 0; j = 0; k = 0; length = 0;
-	char *formatter;
-
-	struct fStruct
-	{
-		char fAbbrev;
-		char *fFull;
-	}
-
-	struct fStruct[] =
-	{
-		{'c', "char"},
-		{'i', "int"},
-		{'f', "float"},
-		{'s', "char *"}
-	}
-		
-	/** loop format to check for c, i, f, s and get length **/
+	int i = 0;
+	va_list argList;
+	va_list listCopy;
+	/** initialize argList with arg_start **/
+	va_start(argList, format);
+	/** loop through format string **/
 	i = 0;
 	while (format[i])
 	{
-		if (format[i] == 'c' || format[i] == 'i' \
-		    || format[i] == 'f' || format[i] == 's')
-			length++;
+		switch (format[i])
+		{
+		case ('c'):
+			printf("%c", va_arg(argList, int));
+			break;
+		case ('i'):
+			printf("%i", va_arg(argList, int));
+			break;
+		case ('f'):
+			printf("%f", va_arg(argList, double));
+			break;
+		case ('s'):
+			va_copy(listCopy, argList);
+			if (va_arg(listCopy, char *))
+				printf("%s", va_arg(argList, char *));
+			else
+				printf("(nil)");
+			va_end(listCopy);
+		}
+		if (format[i + 1])
+		{
+			printf(", ");
+		}
 		i++;
 	}
-			
-	/** declare list of type va_list **/
-	va_list argList;
-
-	/** initialize list using the index of format for the format **/
-	va_start(argList, length);
-
-	/** loop through argList and use printf format specifier **/
-	j = 0;
-	k = 0;
-	while (j < length)
-	{
-		/** if argument is NULL print (nil)
-		 and advance both counters**/
-		
-		
-		/** else if formatter is in struct list **/
-		// print the argument with the formatter
-		// advance the both counters
-		if (format[k] == fStruct[j].fAbbrev)
-		{
-			printf("%*", format[k], fStruct[j].fFull)
-			
-			j++;
-			k++;
-		}
-
-		/** else advance format counter and not argument counter **/
-		    
-
-		 
-	}
+	printf("\n");
+	va_end(argList);
+}
