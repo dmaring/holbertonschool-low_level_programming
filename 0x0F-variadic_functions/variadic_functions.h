317c6aec519a95d2a25b055c8c13785db063d2da
Drew <andrew.maring@gmail.com>
2019-03-17 07:33:45 +0000

    Add description for formatter struct in variadic_functions.h

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -1,10 +1,27 @@
 #ifndef _VARIADIC_FUNCTIONS
 #define _VARIADIC_FUNCTIONS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+
+/**
+ * struct formatter - structure to hold format initial and format function
+ * @fShort: first initial of type format
+ * @fFunc: pointer to function corresponding to format
+ *
+ */
+typedef struct formatter
+{
+	char fShort;
+	void (*fFunc)(va_list);
+} format_t;
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
 
 #endif
