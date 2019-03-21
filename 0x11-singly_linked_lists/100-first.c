5161d246a6779c048d5ce8960adeff004d336131
Drew <andrew.maring@gmail.com>
2019-03-21 23:38:36 +0000

    Edit format or betty linter

--- 0x11-singly_linked_lists/100-first.c
+++ 0x11-singly_linked_lists/100-first.c
@@ -5,5 +5,6 @@ void printBeforeMain(void) __attribute__((constructor));
 /** implementation of printBeforeMain **/
 void printBeforeMain(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
