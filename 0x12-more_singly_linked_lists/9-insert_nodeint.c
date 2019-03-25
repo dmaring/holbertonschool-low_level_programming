c73946c224c7487baec508d6b24b6eb04be84323
Drew <andrew.maring@gmail.com>
2019-03-25 23:04:10 +0000

    Add 9-insert_nodeint.c and add prototype to lists.h

---
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - insert a node at a given position
+ * @head: double pointer to first item in linkedlist
+ * @idx: index of the list where node is added
+ * @n: number to insert into new element node
+ *
+ * Return: address of new node or NULL if failure
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int i;
+
+	listint_t *temp = *head;
+	listint_t *new_node = malloc(sizeof(listint_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+
+	i = 0;
+	while (i < (idx - 1) && temp->next != NULL)
+	{
+		temp = temp->next;
+		i++;
+	}
+
+	if (i == (idx - 1))
+	{
+		new_node->next = temp->next;
+		temp->next = new_node;
+		return (new_node);
+	}
+
+	return (NULL);
+}
