e871df11577c0660a794dd3c5ab0b8019a70c455
Drew <andrew.maring@gmail.com>
2019-03-25 20:38:37 +0000

    Add 5-free_listint2.c and add prototype to lists.h

---
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free a listint_t list as well as head pointer
+ * @head: double pointer to head of the list
+ *
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *next_node;
+
+	while (*head != NULL)
+	{
+		next_node = (*head)->next;
+		free(*head);
+		*head = next_node;
+	}
+
+	head = NULL;
+}
