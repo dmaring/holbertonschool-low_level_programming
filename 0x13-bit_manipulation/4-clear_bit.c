bfde5c1d4ef5f5d03c67408db23e2801322b836c
Drew <andrew.maring@gmail.com>
2019-03-29 01:51:18 +0000

    Fix logic for *n

--- 0x13-bit_manipulation/4-clear_bit.c
+++ 0x13-bit_manipulation/4-clear_bit.c
@@ -14,6 +14,6 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	if (index > (sizeof(unsigned long int) * 8 - 1))
 		return (-1);
 	bit = ~(1 << index);
-	*n = *n | bit;
+	*n = *n & bit;
 	return (1);
 }
