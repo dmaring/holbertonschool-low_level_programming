e5aac4cf780e5cdf3d058606bcedf8a09e3dec5a
Drew <andrew.maring@gmail.com>
2019-03-29 01:37:39 +0000

    Add description for parameter 'index'

--- 0x13-bit_manipulation/2-get_bit.c
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -3,6 +3,7 @@
 /**
  * get_bit - return the value of a bit at the specified index
  * @n: input unsigned number
+ * @index: index target of bit
  *
  * Return: value of bit at index or -1 if an error occurred
  */
