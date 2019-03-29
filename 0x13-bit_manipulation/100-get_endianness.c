3f31492bdfaf3d3325bcc1e6c65db07b2042a01f
Drew <andrew.maring@gmail.com>
2019-03-29 13:28:29 +0000

    Add 100-get_endianness.c

---
+++ 0x13-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * get_endianness - checks the endianness of the machine
+ *
+ * Return: 1 if little endian or 0 if big endian
+ */
+int get_endianness(void)
+{
+	unsigned int i = 1;
+	char *c = (char*) &i;
+	if (*c)
+		return (1);
+	else
+		return (0);
+}
