464dca9724b709bd45462e74d930d36fb1256236
Drew <andrew.maring@gmail.com>
2019-04-02 23:15:57 +0000

    Add 1-create_file.c and add prototype to holberton.h

---
+++ 0x14-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * create_file - function that creates a file
+ * @filename: filename to create
+ * @text_content: text to enter into file
+ *
+ * Return: 1 on success and -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t length;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	if (fd == -1)
+		return (-1);
+
+	length = 0;
+	while (text_content[length] != '\0')
+		length++;
+
+	if (write(fd, text_content, length) == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+
+	close(fd);
+	return (1);
+}
