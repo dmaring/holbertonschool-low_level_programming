af7f0cb9cc34b56952299b30d29133110636f108
Drew <andrew.maring@gmail.com>
2019-04-02 23:46:50 +0000

    Add condition where numwrite != length of string

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -10,24 +10,27 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	ssize_t length;
+	ssize_t length, numwrite;
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
 
 	length = 0;
 	while (text_content[length] != '\0')
 		length++;
 
-	if (text_content != NULL)
-		length = write(fd, text_content, length);
+
+	numwrite = write(fd, text_content, length);
 	close(fd);
 
-	if (length == -1)
+	if (length == -1 || length != numwrite)
 		return (-1);
 
 	return (1);
