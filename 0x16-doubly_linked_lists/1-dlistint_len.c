6dfdd7c4264d79595881bba7c1b6a00182bbe195
Drew <andrew.maring@gmail.com>
2019-05-03 18:59:57 +0000

    Add 1dlistint_len.c and update lists.h with prototype

---
+++ 0x16-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - return number of elements in a linked list
+ * @h: pointer to start of list
+ *
+ * Return: int number of items in list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	int count = 0;
+
+	while(h)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return(count);
+}
