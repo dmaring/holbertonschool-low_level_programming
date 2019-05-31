7c93c2f51ce249629acd5a91b7f637e1cebd8361
Drew <andrew.maring@gmail.com>
2019-05-31 23:39:26 +0000

    Add 1-djb2.c

--- 0x19-hash_tables/1-djb2.c
+++ 0x19-hash_tables/1-djb2.c
@@ -1,18 +1,20 @@
 #include "hash_tables.h"
 
 /**
- * hash_djb2 - implements the djb2 algorithm
+ * hash_djb2 - perform djb2 hash on string input
  * @str: input string to hash
  *
- * Return: hash output
+ * Return: hash as unsigned long int
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-	unsigned long hash = 5381;
+	unsigned long int hash;
 	int c;
 
+	hash = 5381;
 	while ((c = *str++))
+	{
 		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-
+	}
 	return (hash);
 }
