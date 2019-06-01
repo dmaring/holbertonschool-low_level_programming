5f15e302c819706a2cd10cd592e572b197f07bc5
Drew <andrew.maring@gmail.com>
2019-06-01 23:21:26 +0000

    Add 4-hash_table.get.c

---
+++ 0x19-hash_tables/4-hash_table_get.c
@@ -0,0 +1,39 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieve a value based on a given key
+ * @ht: pointer to hash table
+ * @key: input key as string
+ *
+ * Return: value associated with the element or NULL if key not found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int key_idx;
+	hash_node_t **array = ht->array;
+	hash_node_t *seeker;
+
+	/* check if caller passing admissable arguments */
+	if (!ht || !key || !*key)
+		return (NULL);
+
+	/* retrieve hashed key_idx */
+	key_idx = key_index((unsigned char *)key, ht->size);
+
+	/* check if key_idx pointer to node */
+	if (!array[key_idx])
+		return (NULL);
+
+	seeker = array[key_idx];
+	/* loop through matched key slot for matching key string in llist */
+	while (seeker)
+	{
+		if (strcmp(seeker->key, key) == 0)
+			return (seeker->value);
+		seeker = seeker->next;
+	}
+
+	/* key not found */
+	return (NULL);
+
+}
