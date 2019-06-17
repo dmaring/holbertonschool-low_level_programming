040b557a8f3cd52de5b3d4c41872dd85adb5c468
Drew <andrew.maring@gmail.com>
2019-06-17 18:01:45 +0000

    Modify bubble_sort to handle lists of 2 or less

--- 0x1B-sorting_algorithms/0-bubble_sort.c
+++ 0x1B-sorting_algorithms/0-bubble_sort.c
@@ -14,6 +14,9 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j;
 	int temp;
 
+	if (size < 2)
+		return;
+	
 	for (i = 0; i < size - 1; i++)
 		for (j = 0; j < size - 1 - i; j++)
 			if (array[j] > array[j + 1])
