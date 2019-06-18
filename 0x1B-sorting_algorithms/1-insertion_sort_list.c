ef5b09fb4c6e505d0fe64d5e7234d22edd54044d
Drew <andrew.maring@gmail.com>
2019-06-18 22:09:58 +0000

    Edit 1-insertion_sort_list.c for Betty linter

--- 0x1B-sorting_algorithms/1-insertion_sort_list.c
+++ 0x1B-sorting_algorithms/1-insertion_sort_list.c
@@ -24,19 +24,14 @@ void insertion_sort_list(listint_t **list)
 		target_next = target->next;
 		while (c->prev && (c->n < c->prev->n))
 		{
-			/* setup pointers for sanity */
 			cp = c->prev;
 			cn = c->next;
 			temp_cp_prev = cp->prev;
-
-			/* set new values for cp */
 			cp->prev = c;
 			cp->next = cn;
-
 			/* check whether next node is NULL */
 			if (cn)
 				cn->prev = cp;
-
 			c->next = cp;
 			/* check whether previous node is NULL */
 			if (temp_cp_prev)
