d4deca4af26fa756db6201df987e6b823dd9809e
Drew <andrew.maring@gmail.com>
2019-06-19 17:41:29 +0000

    Rearrange checks for NULL values

--- 0x1B-sorting_algorithms/1-insertion_sort_list.c
+++ 0x1B-sorting_algorithms/1-insertion_sort_list.c
@@ -15,7 +15,7 @@ void insertion_sort_list(listint_t **list)
 	listint_t *target_next = NULL;
 	listint_t *temp_cp_prev = NULL;
 
-	if (!(*list) || !(*list)->next || !list)
+	if (!list || !(*list) || !(*list)->next)
 		return;
 	target = (*list)->next;
 	while (target)
@@ -24,16 +24,16 @@ void insertion_sort_list(listint_t **list)
 		target_next = target->next;
 		while (c->prev && (c->n < c->prev->n))
 		{
+			/* setup pointers for sanity */
 			cp = c->prev;
 			cn = c->next;
 			temp_cp_prev = cp->prev;
+			/* set new values for cp */
 			cp->prev = c;
 			cp->next = cn;
-			/* check whether next node is NULL */
 			if (cn)
 				cn->prev = cp;
 			c->next = cp;
-			/* check whether previous node is NULL */
 			if (temp_cp_prev)
 			{
 				c->prev = temp_cp_prev;
