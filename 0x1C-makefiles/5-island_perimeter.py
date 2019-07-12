eea81b7691f3329ced7747dff08fb2bde96296c2
Drew <andrew.maring@gmail.com>
2019-07-12 00:16:58 +0000

    Add 5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -17,23 +17,21 @@ def island_perimeter(grid):
         while cell < len(grid[row]):
             if grid[row][cell] == 1:
                 conn_count = 0
-
-                # check if row is first or very last grid
-                if row == 0 or row == len(grid) - 1:
-                    conn_count += 1
-                # check if cell is first or last in row
-                if cell == 0 or cell == len(grid[row]) - 1:
-                    conn_count += 1
-                # check above, below, left, right of cell
+                # check cell left of current
                 if cell > 0 and grid[row][cell - 1] == 1:
                     conn_count += 1
-                if cell < len(grid[row]) and grid[row][cell + 1] == 1:
+                # check cell right of current
+                if cell < len(grid[row]) - 1 and grid[row][cell + 1] == 1:
                     conn_count += 1
-                if row < len(grid[row]) and grid[row - 1][cell] == 1:
+                # check cell below current
+                if row < len(grid[row]) - 1 and grid[row + 1][cell] == 1:
                     conn_count += 1
-                if row > 0 and grid[row + 1][cell] == 1:
+                # check cell above current
+                if row > 0 and grid[row - 1][cell] == 1:
                     conn_count += 1
                 ret_perim += perimeters[conn_count]
+                # print("Row {} Cell {} has {}".format(row, cell,
+                # perimeters[conn_count]))
             cell = cell + 1
         row = row + 1
 
