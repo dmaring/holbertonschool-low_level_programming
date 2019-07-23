b3a19eed2bf447a710aa7692700dd9be240dc898
Drew <andrew.maring@gmail.com>
2019-07-23 19:41:44 +0000

    Add 13-binary_tree_nodes.c

--- 0x1D-binary_trees/13-binary_tree_nodes.c
+++ 0x1D-binary_trees/13-binary_tree_nodes.c
@@ -1,11 +1,26 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_nodes -
+ * binary_tree_nodes - a function that counts the nodes with at
+ * least 1 child in a binary tree
+ * @tree: root node of the binary tree
  *
- * Return: 
+ * Return: return the number of leaves or 0 if tree is NULL and
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
+	if (!tree)
+		return (0);
 
+	else
+		if (tree->left || tree->right)
+		{
+			return (binary_tree_nodes(tree->left) + 1 +
+				 binary_tree_nodes(tree->right));
+		}
+		else
+		{
+			return (binary_tree_nodes(tree->left) +
+				binary_tree_nodes(tree->right));
+		}
 }
