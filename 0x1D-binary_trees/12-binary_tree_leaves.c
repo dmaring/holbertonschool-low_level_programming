2803a3be6463c395f5dcbc1bae569d1944bb4484
Drew <andrew.maring@gmail.com>
2019-07-23 18:44:06 +0000

    Add 12-binary_tree_leaves.c

--- 0x1D-binary_trees/12-binary_tree_leaves.c
+++ 0x1D-binary_trees/12-binary_tree_leaves.c
@@ -1,11 +1,19 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_leaves -
+ * binary_tree_leaves - a function that counts the leaves in a binary tree
+ * @tree: root of the binary tree
  *
- * Return: 
+ * Return: return the number of leaves or 0 if tree is NULL and
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
+	if (!tree)
+		return (0);
 
+	if (!tree->left && !tree->right)
+		return (1);
+
+	return (binary_tree_leaves(tree->left) +
+		binary_tree_leaves(tree->right));
 }
