697ea498830a878ab5a28f2829652620c085af26
Drew <andrew.maring@gmail.com>
2019-07-24 04:46:55 +0000

    Edit 10-binary_tree_depth.c to return 0 on NULL tree

--- 0x1D-binary_trees/10-binary_tree_depth.c
+++ 0x1D-binary_trees/10-binary_tree_depth.c
@@ -1,6 +1,27 @@
 #include "binary_trees.h"
 
 /**
+ * binary_depth - function that measures the depth of a node in
+ * a binary tree
+ * @tree: pointer to the node to measure the depth
+ *
+ * Return: depth of the binary tree at pointer tree
+ */
+size_t binary_depth(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (-1);
+
+	else
+	{
+		int depth = binary_depth(tree->parent);
+
+		return (depth + 1);
+	}
+}
+
+
+/**
  * binary_tree_depth - function that measures the depth of a node in
  * a binary tree
  * @tree: pointer to the node to measure the depth
@@ -10,12 +31,7 @@
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	if (!tree)
-		return (-1);
+		return (0);
 
-	else
-	{
-		int depth = binary_tree_depth(tree->parent);
-
-		return (depth + 1);
-	}
+	return (binary_depth(tree));
 }
