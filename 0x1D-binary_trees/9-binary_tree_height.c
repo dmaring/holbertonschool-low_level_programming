1539748351089ba0dff546352de23026c52e899a
Drew <andrew.maring@gmail.com>
2019-07-24 03:58:20 +0000

    Edit 9-binary_tree_height.c to return 0 on NULL

--- 0x1D-binary_trees/9-binary_tree_height.c
+++ 0x1D-binary_trees/9-binary_tree_height.c
@@ -9,16 +9,23 @@
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (!tree)
-		return (-1);
+		return (0);
 
 	else
 	{
 		int left_height = binary_tree_height(tree->left);
 		int right_height = binary_tree_height(tree->right);
 
+		if (!tree->right && !tree->left)
+		{
+			left_height--;
+			right_height--;
+		}
+
 		if (left_height > right_height)
 			return (left_height + 1);
 		else
 			return (right_height + 1);
+
 	}
 }
