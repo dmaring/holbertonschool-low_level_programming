4b381e61058a2722098b4a88c8d469d31357583c
Drew <andrew.maring@gmail.com>
2019-07-25 18:13:51 +0000

    Edit 16-binary_tree_is_perfect.c to check form maximum depth

--- 0x1D-binary_trees/16-binary_tree_is_perfect.c
+++ 0x1D-binary_trees/16-binary_tree_is_perfect.c
@@ -1,5 +1,6 @@
 #include "binary_trees.h"
 
+
 /**
  * binary_height - return heigh of binary tree
  * @tree: root of tree
@@ -24,39 +25,62 @@ int binary_height(const binary_tree_t *tree)
 }
 
 /**
- * binary_tree_height - function that measures the height of a binary tree
- * @tree: pointer to the root node of the tree to measure the height
+ * binary_depth - function that measures the depth of a node in
+ * a binary tree
+ * @tree: pointer to the node to measure the depth
  *
- * Return: height of the tree
+ * Return: depth of the binary tree at pointer tree
  */
-size_t binary_tree_height(const binary_tree_t *tree)
+size_t binary_depth(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (-1);
+
+	else
+	{
+		int depth = binary_depth(tree->parent);
+
+		return (depth + 1);
+	}
+}
+
+
+/**
+ * binary_tree_depth - function that measures the depth of a node in
+ * a binary tree
+ * @tree: pointer to the node to measure the depth
+ *
+ * Return: depth of the binary tree at pointer tree
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
 
-	else
-		return (binary_height(tree));
+	return (binary_depth(tree));
 }
 
 /**
- * binary_tree_is_full - a function that checks if a binary tree is full
+ * binary_tree_full - a function that checks if a binary tree is full
  * @tree: root node of the tree to check
+ * @max_depth: maximum depth of the tree
  *
  * Return: 1 if tree is full, 0 if not, or 0 if tree is NULL
  */
-int binary_tree_is_full(const binary_tree_t *tree)
+int binary_tree_full(const binary_tree_t *tree, size_t max_depth)
 {
 	if (!tree)
 		return (0);
 
 	/* check node first for children */
-	if (!tree->left && !tree->right)
+	if ((!tree->left && !tree->right) &&
+	    (binary_tree_depth(tree) == max_depth))
 		return (1);
 	/* check node for both children */
 	else if (tree->left && tree->right)
 		/* check node's children recursively */
-		if (binary_tree_is_full(tree->left) &&
-		    binary_tree_is_full(tree->right))
+		if (binary_tree_full(tree->left, max_depth) &&
+		    binary_tree_full(tree->right, max_depth))
 			return (1);
 
 	return (0);
@@ -70,23 +94,21 @@ int binary_tree_is_full(const binary_tree_t *tree)
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int height_left = 0;
-	int height_right = 0;
-	int matching_height = 0;
+	size_t max_depth = 0;
 	int full_tree  = 0;
 
 	if (!tree)
 		return (0);
 
-	height_left = binary_tree_height(tree->left);
-	height_right = binary_tree_height(tree->right);
+	max_depth = binary_height(tree);
 
-	/* check left and right subtrees for matching height */
-	matching_height = (height_left == height_right) ? 1 : 0;
-	/* check binary tree from root is a full tree */
-	full_tree = (binary_tree_is_full(tree)) ? 1 : 0;
+	/*
+	 * check binary tree from root is a full tree each leaf depth
+	 *  matches max_depth of the tree
+	 */
+	full_tree = (binary_tree_full(tree, max_depth)) ? 1 : 0;
 
-	if (matching_height && full_tree)
+	if (full_tree)
 		return (1);
 	else
 		return (0);
